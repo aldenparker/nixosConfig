dac4f0bc86510afb22ba00a792c096b4c35ab59d
Alden Parker <ajparker1401@gmail.com>
2025-02-08 11:45:56 -0500

    add htop as system package

--- nixos/mobile-server/configuration.nix
+++ nixos/mobile-server/configuration.nix
@@ -12,7 +12,10 @@
 
   # --- Setup snowman modules (my custom modules)
   snowman = {
-    programs = { zsh.enable = true; };
+    programs = { 
+      zsh.enable = true; 
+      htop.enable = true;
+    };
 
     services = {
       glance = {
