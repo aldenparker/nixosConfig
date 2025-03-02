0b4b3eea7e9dd18402f7f412d982a3f60a279f32
Alden Parker <ajparker1401@gmail.com>
2025-03-01 22:51:01 -0500

    fix zig devshell

--- shells/zig-master/default.nix
+++ shells/zig-master/default.nix
@@ -9,14 +9,12 @@
 # Create zig-master shell
 let
   zig = inputs.zig-overlay.packages.${system}.master;
-  zls = inputs.zls-overlay.packages.${system}.zls.overrideAttrs (old: {
-    nativeBuildInputs = [ zig ];
-  });
+  zls = inputs.zls-overlay.packages.${system}.zls;
 in
 mkShell {
   packages = with pkgs; [
     zig
-    # zls # TODO: fix zls build with help of zigget post https://ziggit.dev/t/zls-on-nixos-fails-to-build/8810
+    zls
     lldb
   ];
 }
